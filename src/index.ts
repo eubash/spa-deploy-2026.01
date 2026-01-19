d39c00d51ce0bb0cf9d3ebf0da6a908d4f23208f
Evgeniya Bashieva <eugenia.bashieva@remago.com>
2026-01-19 18:26:56 +0100

    update history api

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,21 @@ function render(hash?: string) {
       : window.location.href;
 }
 
-// if (PRODUCTION) {
-//   document.querySelectorAll("a").forEach((link) => {
-//     link.href = PREFIX + link.pathname;
-//   });
-// }
+if (PRODUCTION) {
+  document.querySelectorAll("a").forEach((link) => {
+    link.href = PREFIX + link.pathname;
+  });
+}
 
 document.body.addEventListener("click", (ev) => {
   if ((ev.target as HTMLElement).matches("a")) {
-    const hash = (ev.target as HTMLAnchorElement).hash;
+    ev.preventDefault();
 
-    render(hash);
+    const url = (ev.target as HTMLAnchorElement).href;
+
+    window.history.pushState({}, '', url);
+
+    render();
   }
 });
 
